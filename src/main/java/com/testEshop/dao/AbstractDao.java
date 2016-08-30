a840ba71e16858c860e1ddbfea5eaea47f606fb1
vasylenkoMO <vasyl2.71nko@gmail.com>
2016-08-30 10:22:40 +0300

    changed save method and now it is avalible to cmplete registration successfully

--- src/main/java/com/testEshop/dao/AbstractDao.java
+++ src/main/java/com/testEshop/dao/AbstractDao.java
@@ -52,7 +52,7 @@ public abstract class AbstractDao <PK extends Serializable, T> {
     }
 
     public void persist(T entity) {
-        getSession().persist(entity);
+        getSession().save(entity);
     }
 
     public void delete(T entity) {
